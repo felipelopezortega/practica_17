eea431a95ae07e590c48c4639518203dbe1385e9
felipelopezortega <flopezortega@ucol.mx>
2022-05-16 22:49:29 -0500

    the database is connected

--- app.js
+++ app.js
@@ -24,4 +24,21 @@ app.get("/paroimpar/:numero", function(req, res){
     res.render("numero", {NUMBER: req.params.numero});
 });
 
+const { MongoClient, ServerApiVersion } = require("mongodb"); // Iinyectando dependencias de mongo
+const uri = //Incluimos el enlace de conexion
+  "mongodb+srv://felipe:2336915457@cluster0.xgnn0.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+}); //generamos el clinete
+client
+  .connect() //Nos conectamos a la base de datos
+  .then(() => {
+    console.log("Connected to the database "); // si la conexion es valida mandamos el mensaje
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. n${err}`); // Si no se puede conectar mandamos el error
+  });
+
 app.listen(port); //levantar el server y ponerlo a la escucha
